d29091ecc6190fd511f2d01f88388c1539d98b62
zhangsx19 <zhangsx19@mails.tsinghua.edu.cn>
2022-08-15 19:17:55 +0800

    change list-files

--- hw-test/frontend/frontend/src/views/HomeView.vue
+++ hw-test/frontend/frontend/src/views/HomeView.vue
@@ -40,20 +40,40 @@ export default {
     }, 2000);
   },
   methods: {
+    list2tree(files) {
+      var tree_arr = [];
+      for (var i = 0; i < files.length; i++) {
+        var tree = {};
+        tree["label"] = files[i]["name"];
+        if (files[i]["folder"] == 1) {
+          tree["children"] = this.list2tree(files[i]["files"]);
+        }
+        tree_arr.push(tree);
+      }
+      return tree_arr;
+    },
     refreshlist() {
       $.ajax({
-        url: "http://127.0.0.1:5000/project/list-files",
+        url: "http://127.0.0.1:4523/m1/1454888-0-default/project/list-files",
         type: "POST",
         data: JSON.stringify({
-          userid: "1",
-          projectid: "a",
+          session: "1",
+          project: "a",
         }),
         dataType: "json",
         success: (resp) => {
-          // console.log(resp);
+          //console.log(resp);
           this.data = [];
-          this.data.push(resp);
-          console.log(this.data);
+          if (resp["code"] == 0) {
+            console.log(this.list2tree(resp["files"]));
+            this.data = this.list2tree(resp["files"]);
+          } else if (resp["code"] == 1) {
+            alert("session无效");
+          } else if (resp["code"] == 2) {
+            alert("session无效");
+          } else {
+            alert("未知错误");
+          }
         },
       });
     },
